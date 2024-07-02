ab85c86eea67b4faefd72605380642f0e33d4bdf
Brian Obot <brianobot9@gmail.com>
2024-07-02 16:22:52 +0100

    Update codebase

---
+++ src/bin/10_b2_more_enums.rs
@@ -0,0 +1,70 @@
+#![allow(dead_code)]
+
+fn main() {
+    // enums variant can hold any type and amount of associate data
+
+    #[derive(Debug, Clone)]
+    struct PlayerData {
+        name: String,
+        health: i32,
+    }
+
+    impl PlayerData {
+        fn is_weak(&self) -> bool {
+            return &self.health < &30;
+        }
+
+        fn is_strong(&self) -> bool {
+            return &self.health > &70;
+        }
+
+        fn is_dead(&self) -> bool {
+            return &self.health == &0;
+        }
+    }
+
+    #[derive(Debug)]
+    enum Player {
+        Regular(PlayerData),
+        Advance(PlayerData),
+        NPC(PlayerData),
+    }
+
+    impl Player {
+        fn intro(&self) {
+            let name = match &self {
+                Player::Regular(_) => "Regular Player",
+                Player::Advance(_) => "Advance Player",
+                Player::NPC(_) => "NPC Player",
+            };
+            println!("Hello! I am a {}", name);
+        }
+    }
+
+    let player_data = PlayerData { 
+        name: String::from("Brian"), 
+        health: 100 
+    };
+
+    let is_old = player_data.is_weak();
+    let is_dead = player_data.is_dead();
+    let is_strong = player_data.is_strong();
+    println!("The player is weak = {}", is_old);
+    println!("The player is strong = {}", is_strong);
+    println!("The player is dead = {}", is_dead);
+
+    let regular_player = Player::Regular(player_data.clone());
+    let advance_player = Player::Advance(player_data.clone());
+    let npc = Player::NPC(player_data.clone());
+
+    println!("Advance Player = {:?}", advance_player);
+    println!("Regular Player = {:?}", regular_player);
+    println!("NPC = {:?}", npc);
+
+    regular_player.intro();
+    advance_player.intro();
+    npc.intro();
+
+
+
+}
