b4a391ac70e8068131ef87bc75cd7bee7cd66a4f
Brian Obot <brianobot9@gmail.com>
2024-10-09 15:14:37 +0100

    Updates

--- src/bin/04_comments.rs
+++ src/bin/04_comments.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
+
 fn main() {
     // usually you would find them used above the line which they are referring to 
     // funny how i started using comments since the first lesson, lol, Bye
@@ -5,4 +7,16 @@ fn main() {
     /*
     these are multiline comments.. now bye for real
      */
+
+    // we also have doc comments, that are use in the auto generated docs for rust codes
+    /// Line comments; document the next item
+    /** Block comments; document the next item */
+    struct SomeStruct;
+
+    mod module {
+        //! Line comments; document the enclosing item
+        /*! Block comments; document the enclosing item !*/
+
+    }
+    
 }
--- src/bin/25_conditional_compilation.rs
+++ src/bin/25_conditional_compilation.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
+
 fn main() {
     // to add condition compilation to your project
     // annotate the source code with cfg attributes, 
