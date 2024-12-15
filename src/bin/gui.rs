088206d3b7e1348400654372007e7a144acc7905
Brian Obot <brianobot@gmail.com>
2024-12-15 08:48:15 +0100

    Add lesson for OOP in rust with trait objects

---
+++ src/bin/gui.rs
@@ -0,0 +1,53 @@
+/*
+this file shows the use of OOP approach to building a gui application in rust
+*/
+
+pub trait Draw {
+    fn draw(&self) {}
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+
+pub struct Button {
+    width: u32,
+    height: u32,
+    label: String,
+}
+
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Drawing Button height: {}, width: {}, label: {}", self.height, self.width, self.label);
+    }
+}
+
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("Close"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
