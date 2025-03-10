2539dbdc4d6cb00b9cde5679c991d6950175dc41
Brian Obot <brianobot@gmail.com>
2025-03-10 15:43:18 +0100

    Add examples demonstrating operator overloading and unsafe Rust features

--- src/bin/36_operator_overloading.rs
+++ src/bin/36_operator_overloading.rs
@@ -3,6 +3,9 @@ this is the process of overloading the operators functionalities for some types 
 examples of operators are add (+) sub (-) modulus (%) etc
 
 operators that can be overloaded are available in the std::ops module
+
++ is a syntactic sugar for the add method
+1u32 + 2u32 === 1u32.add(2u32)
 */ 
 #![allow(unused)]
 
@@ -47,6 +50,7 @@ struct Divorce {
     initiator: Person,
 }
 
+// supporting Marriage - Person == Divorce
 impl Sub<Person> for Marriage {
     type Output = Divorce;
     
@@ -58,6 +62,17 @@ impl Sub<Person> for Marriage {
     }
 }
 
+impl Add<i32> for Marriage {
+    type Output = Marriage;
+    
+    // it's important to note that the trait definition doesn't
+    // have to match the trait method argument names, but the types must match
+    fn add(self, other: i32) -> Self::Output {
+        self
+    }
+
+    
+}
 
 
 fn main() {
@@ -67,6 +82,9 @@ fn main() {
     let marriage = peter + sarah.clone();
     println!("Marriage = {:?}", marriage);
 
-    let divorce = marriage - sarah;
+    let divorce = marriage.clone() - sarah;
     println!("Divorce = {:?}", divorce);
+
+    let marrage_upgrade = marriage + 1i32;
+    println!("Marriage Upgrade = {:?}", marrage_upgrade);
 }
---
+++ src/bin/37_unsafe_rust_intro.rs
@@ -0,0 +1,104 @@
+/*
+Rust offers the ability to write unsafe codes
+
+
+things that can be done in unsafe rust
+- deference a raw pointer
+- call and unsaf function or method
+- access or modify static mutable variable
+- implement unsafe trait
+- access fields of union
+
+
+NOTE: unsafe does not turn off the borrow checker 
+*/
+#![allow(unused)]
+
+
+static HELLO_WORD: &str = "Hello, World!";
+static mut COUNTER: u32 = 0;
+
+
+fn add_to_counter(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
+
+fn main() {
+    let mut num = 5;
+
+    // 1.
+    // this is an immutable raw pointer that points to a i32 value
+    // the asterisk is not not 
+    let r1 = &num as *const i32;
+    // this is a mutable raw pointer that points to a i32 value
+    let r2 = &mut num as *mut i32;
+
+    // raw pointers do not follow rust borrow checker rules
+    // they are not guarantee to point a valid value
+
+    // rust allows us to create raw pointers in the context of sage rust
+    // but in order to deference the raw pointers,that must be done within an unsafe block
+
+    unsafe {
+        // notice how we created a mutable and an immutable raw pointer to the same address
+        // in regular references in rust, this is not possible
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    // 2.
+    // calling unsafe functions or methods
+    unsafe fn dangerous() {}
+
+    unsafe {
+        dangerous();
+    }
+
+    // unsafe functions must be called within unsafe blocks
+    // or within other unsafe functions
+
+    // implementating safe abstraction
+    // we can have safe functions that have unsafe implementations in them but can be called and used as regular
+    // safe functions
+
+
+    // Extern functions to call external code
+    extern "C" {
+        fn abs(input: i32) -> i32;
+    }
+
+
+    unsafe {
+        println!("Absolute value of -3 according to C: {}", abs(-3));
+    }
+
+    // 3. Access and Modify mutable stable variable
+    add_to_counter(3);
+    unsafe {
+        println!("COUNTER: {}", COUNTER);
+    }
+
+    // 4. Implementation of Unsafe trait
+    // a trait is unsafe when atleast one of it method is unsafe
+
+    unsafe trait Foo {}
+
+    unsafe impl Foo for i32 {}
+
+    // Union
+    union Win {
+        bronze: i32,
+        silver: i32,
+        gold: i32,
+    }
+
+
+
+    let win = Win { silver: 30 };
+
+    unsafe {
+        println!("WIn field: {:?}", win.silver);
+    }
+}
