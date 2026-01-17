8a10688ae54910fe9de85dca333b80100f5a4d76
Brian Obot <brianobot@gmail.com>
2026-01-17 21:00:43 +0100

    Add More notes on Arrays and Slices

--- src/bin/02b_1_arrays.rs
+++ src/bin/02b_1_arrays.rs
@@ -13,4 +13,30 @@ fn main() {
     // all sequence here, array, vector and slices have a len method
     // and their values can be indexed with sequence[i] where i must be a usize value
     // if the value is out of range rust will panics
+    // 
+    // you can initialize an array with a short hand like so
+    let zeros = [0; 100];
+    println!("Zeros: {zeros:?}");
+    
+    let mut sieve = [true; 1000];
+    for i in 2..100 {
+        if sieve[i] {
+            let mut j = i * i;
+            while j < 1000 {
+                sieve[j] = false;
+                j += 1;
+            }
+        }
+    }
+    
+    dbg!(sieve);
+    
+    // you would use similar syntax for fixed size buffer used in codes
+    let _fixed_size_buffer = [0u8; 1024];
+    
+     // the helpful methods you might need for iterating, sorting etc are all provided on slices and not arrays
+     // but rust implicity converts a reference to an array to a slice when searching for methods
+     let mut chaos = [1, 2, 5, 5, 2, 4, 6, 3, 6, 7];
+     chaos.sort();
+     println!("Sorted Chaos: {chaos:?}");
 }
