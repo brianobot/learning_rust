7b6d438d5d5d164a089a93a4c26521bffdf50d7a
Brian Obot <brianobot@gmail.com>
2026-01-22 17:34:02 +0100

    Add Notes on Result

---
+++ src/bin/03ba_closures.rs
@@ -0,0 +1,9 @@
+
+
+fn main() {
+    // a closure is usually consisted of an arugment list given between vertical bars followed by an expression
+    let is_even = |x| -> bool { x & 1 == 0 };
+    
+    let result = is_even(2);
+    println!("Is Even: {result}");
+}
---
+++ src/bin/40_error_handling.rs
@@ -0,0 +1,65 @@
+#![allow(dead_code)]
+
+use tokio::io;
+
+
+
+fn main() {
+      // panics represent something that should never happen
+      // if you compile a rust program with the flag -C panic=abort
+      // rust does not unwind the stack when a panic is met, it just aborts, this reduces the binary sizes of compile programs
+      // because the stack info is not included
+      
+      
+      // let cap_share = pirate_share(100, 0);
+      // println!("Cap Share: {cap_share}");
+      // 
+      // Result type
+      // this represent possible failure
+      // 
+      // catching result can be done with match expression
+      let weather_report = get_weather(LatLng(0.1, 1.));
+      
+      match weather_report {
+        Ok(info) => println!("Found Weather Report: {}", info.0),
+        Err(_) => println!("Could Not Find Weather Report... Try Again"),
+    }
+    
+    let weather_report_2 = get_weather(LatLng(10., 10.));
+    if weather_report_2.is_ok() {
+        println!("Weather Report 2 is Available!");
+    } else {
+        println!("Weather Report 2 is Not Available ⚠️");
+    }
+    
+    let weather_report_3 = get_weather(LatLng(02., 23.));
+    let some_value = weather_report_3.clone().ok(); // converts Result<T, E> -> Option<T>
+    let err_value = weather_report_3.clone().err();
+    let fallback = weather_report_3.clone().unwrap_or(WeatherReport("Default Weather Report".to_string()));
+    
+    println!("Some_value = {some_value:?}");
+    println!("Error Value = {err_value:?}");
+    println!("Fallback = {fallback:?}");
+    
+    
+    
+    
+}
+
+
+#[allow(dead_code)]
+fn pirate_share(total: u64, crew_size: usize) -> u64 {
+    let half = total / 2;
+    half / crew_size as u64
+}
+
+
+struct LatLng(f64, f64);
+
+#[derive(Debug, Clone)]
+struct WeatherReport(String);
+
+
+fn get_weather(_location: LatLng) -> Result<WeatherReport, String> {
+    Ok(WeatherReport(format!("Info at Coordination({}, {})", _location.0, _location.1)))
+}
