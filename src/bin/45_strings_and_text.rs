a36fd467788bfedc1d207586f9ddf48ed280ed1c
Brian Obot <brianobot@gmail.com>
2026-03-13 09:02:01 +0100

    Add notes on Strings

--- src/bin/45_strings_and_text.rs
+++ src/bin/45_strings_and_text.rs
@@ -8,4 +8,25 @@ fn main() {
     for (index, i) in s.bytes().enumerate() {
         println!("{index}: {}", i as char);
     }
+
+    // given a String, you can display it UTF-8 bytes with the bytes method
+    let text = String::from("Hello Brian");
+    let bytes = text.as_bytes();
+    for byte in bytes {
+        println!("{}", byte);
+    }
+
+    // Char are 32 bit (4 bytes) types that represent a single character
+    // methods available on char
+    // .is_numeric()
+    // .is_alphabetic()
+    // .is_alphanumeric()
+    // .is_whitespace()
+    // .is_control()
+    //
+    // Since u8 overlap over the first 128 Ascii characters
+    // rust provides char methods on u8 types prepended with is_ascii_
+    // example of these methods include, these methods are also available on char too
+    // .is_ascii_alphabetic() ...
+    //
 }
